a6e20af31bf7a9143730cfcb6aff04443d8d1962
MatthewSawatz <msawatzky2@rrc.ca>
2024-08-12 18:24:33 -0500

    more skills added

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -5,32 +5,36 @@ import SkillList from "../../common/SkillList";
 function Skills() {
   return (
     <section id="skills" className={styles.container}>
-      <h1 className='sectionTitle'>Skills</h1>
+      <h1 className="sectionTitle">Skills</h1>
       <div className={styles.skillList}>
         <SkillList src={checkMarkIcon} skill="HTML" />
         <SkillList src={checkMarkIcon} skill="CSS" />
         <SkillList src={checkMarkIcon} skill="JavaScript" />
         <SkillList src={checkMarkIcon} skill="TypeScript" />
-        <SkillList src={checkMarkIcon} skill="Node" />
-        <SkillList src={checkMarkIcon} skill="GoLang" />
+        <SkillList src={checkMarkIcon} skill="Python" />
+        <SkillList src={checkMarkIcon} skill="Ruby" />
+        <SkillList src={checkMarkIcon} skill="Java" />
+        <SkillList src={checkMarkIcon} skill="Go" />
+        <SkillList src={checkMarkIcon} skill="PHP" />
       </div>
       <hr />
       <div className={styles.skillList}>
         <SkillList src={checkMarkIcon} skill="React" />
-        <SkillList src={checkMarkIcon} skill="Tailwind CSS" />
-        <SkillList src={checkMarkIcon} skill="JavaScript" />
-        <SkillList src={checkMarkIcon} skill="TypeScript" />
+        <SkillList src={checkMarkIcon} skill="Tailwind" />
+        <SkillList src={checkMarkIcon} skill="Flask" />
         <SkillList src={checkMarkIcon} skill="Node" />
-        <SkillList src={checkMarkIcon} skill="GoLang" />
+        <SkillList src={checkMarkIcon} skill="Rails" />
+        <SkillList src={checkMarkIcon} skill="SQL" />
       </div>
       <hr />
       <div className={styles.skillList}>
         <SkillList src={checkMarkIcon} skill="Git" />
         <SkillList src={checkMarkIcon} skill="Docker" />
         <SkillList src={checkMarkIcon} skill="AWS" />
-        <SkillList src={checkMarkIcon} skill="TypeScript" />
-        <SkillList src={checkMarkIcon} skill="Node" />
-        <SkillList src={checkMarkIcon} skill="GoLang" />
+        <SkillList src={checkMarkIcon} skill="Google Cloud" />
+        <SkillList src={checkMarkIcon} skill="PhpMyAdmin" />
+        <SkillList src={checkMarkIcon} skill="Storybook" />
+        <SkillList src={checkMarkIcon} skill="Postman" />
       </div>
     </section>
   );
