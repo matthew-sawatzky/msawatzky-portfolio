eb5779e005832f6fee2b8c7246178e35a73e8ed6
MatthewSawatz <msawatzky2@rrc.ca>
2024-12-03 13:59:49 -0600

    revised resume and skills changes

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -7,35 +7,29 @@ function Skills() {
     <section id="skills" className={styles.container}>
       <h1 className="sectionTitle">Skills</h1>
       <div className={styles.skillList}>
-        <SkillList src={checkMarkIcon} skill="HTML" />
-        <SkillList src={checkMarkIcon} skill="CSS" />
         <SkillList src={checkMarkIcon} skill="JavaScript" />
         <SkillList src={checkMarkIcon} skill="TypeScript" />
+        <SkillList src={checkMarkIcon} skill="HTML" />
+        <SkillList src={checkMarkIcon} skill="CSS" />
         <SkillList src={checkMarkIcon} skill="Python" />
         <SkillList src={checkMarkIcon} skill="Ruby" />
         <SkillList src={checkMarkIcon} skill="Java" />
-        <SkillList src={checkMarkIcon} skill="Go" />
         <SkillList src={checkMarkIcon} skill="PHP" />
+        <SkillList src={checkMarkIcon} skill="SQL" />
       </div>
       <hr />
       <div className={styles.skillList}>
         <SkillList src={checkMarkIcon} skill="React" />
         <SkillList src={checkMarkIcon} skill="Tailwind" />
-        <SkillList src={checkMarkIcon} skill="Nextjs" />
-        <SkillList src={checkMarkIcon} skill="Flask" />
-        <SkillList src={checkMarkIcon} skill="Node" />
+        <SkillList src={checkMarkIcon} skill="Next.js" />
         <SkillList src={checkMarkIcon} skill="Rails" />
-        <SkillList src={checkMarkIcon} skill="SQL" />
       </div>
       <hr />
       <div className={styles.skillList}>
         <SkillList src={checkMarkIcon} skill="Git" />
         <SkillList src={checkMarkIcon} skill="Docker" />
-        <SkillList src={checkMarkIcon} skill="AWS" />
-        <SkillList src={checkMarkIcon} skill="Google Cloud" />
-        <SkillList src={checkMarkIcon} skill="PhpMyAdmin" />
-        <SkillList src={checkMarkIcon} skill="Storybook" />
         <SkillList src={checkMarkIcon} skill="Postman" />
+        <SkillList src={checkMarkIcon} skill="AWS" />
       </div>
       <br />
       <br />
